1d58e3cfeb25fdd85062c073b446473fd0f71683
lilonghe <lilonghe@outlook.com>
2021-04-03 16:44:16 +0800

    add 支持public下文件打包时自动发布

--- build/webpack.pro.js
+++ build/webpack.pro.js
@@ -1,6 +1,24 @@
 const { merge } = require('webpack-merge');
 const base = require('./webpack.base.js');
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require('path');
 
 module.exports = merge(base, {
     mode: 'production',
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { 
+                    from: 'public',
+                    to: './',  
+                    globOptions: {
+                        ignore: [
+                            path.resolve(__dirname, '../public/', 'index.html'),
+                        ]
+                    }
+                }
+            ],
+            
+        })
+    ]
 });
